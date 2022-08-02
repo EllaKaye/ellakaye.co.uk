43a92011029e87f0962744c367da923ab30a749f
EllaKaye <ella.kaye@gmail.com>
2022-08-02 14:23:59 +0100

    Starting to take shape

---
+++ bootstrap_icons.R
@@ -0,0 +1,15 @@
+# Get icon names
+
+# bootstrap
+
+bi_icons <- jsonlite::fromJSON("https://raw.githubusercontent.com/twbs/icons/main/font/bootstrap-icons.json")
+
+bi_icons |> tibble::enframe()
+
+# fontawesome (piggyback off fontawesome R package)
+
+fa_icons <- fontawesome:::fa_tbl
+
+# academicons
+
+
