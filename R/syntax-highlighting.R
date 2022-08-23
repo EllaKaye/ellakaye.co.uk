f9d5853127136aa1498f9599a0653f35a041a7d7
EllaKaye <ella.kaye@gmail.com>
2022-08-23 16:08:57 +0100

    distill highlighting posts, update syntax highlighting

--- R/syntax-highlighting.R
+++ R/syntax-highlighting.R
@@ -10,7 +10,7 @@ theme_light <- readLines("template-light.theme")
 
 # apply my colours
 theme_light <- gsub("#AD0000", "#B6005B", theme_light) # red -> pink
-theme_light <- gsub("#8f5902", "#B55A00", theme_light) # brown -> brown (note lower case!)
+theme_light <- gsub("#8f5902", "#B55A00", theme_light) # brown -> orange (note lower case!)
 theme_light <- gsub("#00769E", "#5B00B6", theme_light) # blue -> purple
 theme_light <- gsub("#657422", "#5B00B6", theme_light) # light-green -> purple (attributes)
 theme_light <- gsub("#20794D", "#008542", theme_light) # green -> green
@@ -20,7 +20,8 @@ theme_light <- gsub("#f1f3f5", "#E9E9EF", theme_light) # light-gray -> gray-200 
 theme_light <- gsub("#111111", "#212129", theme_light) # dark-gray -> gray-900
 theme_light <- gsub("#003B4F", "#212129", theme_light) # dark-blue -> gray-900
 theme_light <- gsub("#aaaaaa", "#ADADBD", theme_light) # mid-gray -> dark-500 
-# other set manually to #494957 (gray-700) in template-light
+# Other set manually to #494957 (gray-700) in template-light
+# ControlFlow set manually to #B55A00 (orange) in template-light
 
 
 writeLines(theme_light, "ek-light.theme")
@@ -44,8 +45,8 @@ theme_dark  <- gsub("#f1f3f5", "#343440", theme_dark ) # light-gray -> gray-800 
 theme_dark  <- gsub("#111111", "#F8F8FA", theme_dark ) # dark-gray -> gray-100
 theme_dark  <- gsub("#003B4F", "#F8F8FA", theme_dark ) # dark-blue -> gray-100
 theme_dark  <- gsub("#aaaaaa", "#ADADBD", theme_dark ) # mid-gray -> dark-500 
-theme_dark  <- gsub("#494957", "#DCDCE6", theme_dark ) # gray-300 other (assignment)
-
+theme_dark  <- gsub("#494957", "#DCDCE6", theme_dark ) # gray-300 Other (assignment)
+theme_dark  <- gsub("#B55A00", "#FF962C", theme_dark ) # orange ControlFlow
 
 writeLines(theme_dark, "ek-dark.theme")
 
